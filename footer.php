5fd47be88bf5296655758756c457d6f305d00a30
Julia Browne <juliabrowne@Julias-MacBook-Pro-2.local>
2018-08-24 11:48:27 -0700

    init

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+ ?>
+ 
+			</div><!-- #content -->
+ 
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					<?php 
+						wp_nav_menu( 
+					 	array( 
+						 	'theme_location' => 'primary', 
+							 'menu_id' => 'primary-menu',
+							 'menu_class' => 'footer-navigation'
+						 	) 
+						); 
+					?>
+					<p>brought to you by <a href="http://www.github.com/juliabrowne">juliabrowne</a></p>	
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+ 
+		<?php wp_footer(); ?>
+ 
+	</body>
+ </html>
